a020000158f2430fa92de255c1bfa95aaa2a2178
cheek-codes <cheek.codes@gmail.com>
2023-02-11 22:35:03 -0500

    dark mode

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,15 @@
 let menu = document.querySelector("#menu-bars")
 let navbar = document.querySelector(".navbar")
 
+let toggleDarkMode = document.querySelector("#toggle-dark-mode")
+let body = document.querySelector("body")
+let header = document.querySelector("header")
+
+toggleDarkMode.onclick = () => {
+    body.classList.toggle("dark-mode")
+    header.classList.toggle("dark-mode")
+}
+
 menu.onclick = () => {
     menu.classList.toggle("fa-times")
     navbar.classList.toggle("active")
